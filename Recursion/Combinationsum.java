b8076656ac088fc5e2b466a4141f4e6b7fd28e28
Kunal Chandra <kunalchandra2506@gmail.com>
2025-08-06 22:34:58 +0530

    Updates Till August

--- Recursion/Combinationsum.java
+++ Recursion/Combinationsum.java
@@ -55,3 +55,29 @@ public class Combinationsum {
         }
     }
 }
+//For leetcode
+// class Solution {
+//     private  void combinationsumhelper(int ind,int arr[],int target,List<Integer>ds,List<List<Integer>>ans){
+//         if(ind==arr.length){
+//             if(target==0){
+//                 ans.add(new ArrayList<>(ds));
+
+//             }
+//             return;
+//         }
+//         if(arr[ind]<=target){
+//             ds.add(arr[ind]);
+//             combinationsumhelper(ind,arr,target-arr[ind],ds,ans);
+//             ds.remove(ds.size()-1);
+//         }
+//         combinationsumhelper(ind+1,arr,target,ds,ans);
+
+//     }
+
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>>ans=new ArrayList<>();
+//         combinationsumhelper(0,candidates,target,new ArrayList<>(),ans);
+//         return ans;
+        
+//     }
+// }
