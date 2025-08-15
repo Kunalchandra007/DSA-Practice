e5fe586f690844c9a580f3ae34889d9af978b669
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-08-15 23:19:26 +0530

    DSA

---
+++ Recursion/Permutations2.java
@@ -0,0 +1,45 @@
+package Recursion;
+
+import java.util.*;
+
+public class Permutations2 {
+    private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
+        if (index == nums.length) {
+            // copy the ds to ans
+            List<Integer> ds = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++)
+                ds.add(nums[i]);
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            recurPermute(index + 1, nums, ans);
+            swap(i, index, nums);
+        }
+    }
+
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        recurPermute(0, nums, ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Permutations2 obj = new Permutations2();
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> permutations = obj.permute(nums);
+
+        System.out.println("All permutations:");
+        for (List<Integer> perm : permutations) {
+            System.out.println(perm);
+        }
+    }
+}
