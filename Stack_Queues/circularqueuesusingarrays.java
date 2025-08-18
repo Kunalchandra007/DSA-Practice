de6b15495576ba6cdfe2d6a2cd46968fd19fdac0
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-08-18 23:42:04 +0530

    Commit

---
+++ Stack_Queues/circularqueuesusingarrays.java
@@ -0,0 +1,70 @@
+package Stack_Queues;
+
+public class circularqueuesusingarrays {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+        Queue(int n){//constructor
+            arr=new int[n];
+            size=n;
+            rear=-1;
+            front=-1;
+        }
+
+    
+    public static boolean isEmpty(){
+        return rear==-1 && front==-1;
+    }
+    public static boolean isFull(){
+        return(rear+1)%size==front;
+    }
+    //add
+    public static void add(int data){
+        if(isFull()){
+            System.out.println("Queue is full");
+            return;
+
+        }
+        if(front==-1){//case for 1st element
+            front=0;
+        }
+        rear=(rear+1)%size;
+        arr[rear]=data;
+    }
+    public static int remove(){
+        if(isEmpty()){
+            System.out.println("empty queue");
+            return -1; 
+        }
+        int result=arr[front];
+        //last element
+        if(rear==front){
+            rear=front=-1;
+        }else{
+             front=(front+1)%size;
+        }
+        return front;
+    }
+    public static int peek(){
+        if(isEmpty()){
+            System.out.println("empty queue");
+            return -1; 
+        }
+        return arr[front];
+
+    }
+
+}
+public static void main(String[] args) {
+    Queue q=new Queue(5);
+    q.add(2);
+    q.add(5);
+    q.add(7);
+    while(!q.isEmpty()){
+        System.out.println(q.peek());
+        q.remove();
+    }
+}
+}
