0c79fee32dbf9c75bca0daa659487e5f237224cc
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-08-19 23:29:09 +0530

    Commit

---
+++ Stack_Queues/queueusingJCF.java
@@ -0,0 +1,18 @@
+package Stack_Queues;
+
+import java.util.*;
+
+public class queueusingJCF {
+    public static void main(String[] args) {
+        Queue<Integer>q=new LinkedList<>();
+        //Queue<Integer>q=new ArrayDeque<>();
+        q.add(1);
+        q.add(3);
+        q.add(4);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+}
