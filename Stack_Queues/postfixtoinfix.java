a23000c80553fc0e796dffe51b653cf9a0c083f9
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-09-01 20:19:30 +0530

    Commit

---
+++ Stack_Queues/postfixtoinfix.java
@@ -0,0 +1,34 @@
+package Stack_Queues;
+import java.util.*;
+
+public class postfixtoinfix {
+    public static String convert(String exp){
+        Stack<String> st = new Stack<>();
+
+        for(int i=0; i<exp.length(); i++){
+            char c = exp.charAt(i);
+
+            if(Character.isLetterOrDigit(c)){
+                st.push(Character.toString(c));
+            } else {
+                if (st.size() < 2) {
+                    throw new IllegalArgumentException("Invalid postfix expression: " + exp);
+                }
+                String t1 = st.pop();
+                String t2 = st.pop();
+                String result = "(" + t2 + c + t1 + ")";
+                st.push(result);
+            }
+        }
+        if (st.size() != 1) {
+            throw new IllegalArgumentException("Invalid postfix expression: " + exp);
+        }
+        return st.pop();
+    }
+
+    public static void main(String[] args) {
+        String postfix = "ab*c+";  
+        System.out.println("Postfix expression: " + postfix);
+        System.out.println("Converted expression: " + convert(postfix));
+    }
+}
