75ee0a9495b2c0ba9d6a0ba3691a586346f25b77
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-10-16 19:00:31 +0530

    Commit

--- DP/uniquepaths2.java
+++ DP/uniquepaths2.java
@@ -1,23 +1,29 @@
 package DP;
 
 public class uniquepaths2 {
- 
-    static int mod = (int)(1e9 + 7);
 
-    public static int mazeObstacles(int n, int m, int[][] mat) {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+
         int[][] dp = new int[n][m];
 
+        // Base case: starting cell
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+
+        // Fill the dp table
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (mat[i][j] == -1) {
-                    dp[i][j] = 0;
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; // obstacle cell
                 } else if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
+                    dp[i][j] = 1; // already handled
                 } else {
                     int up = 0, left = 0;
                     if (i > 0) up = dp[i - 1][j];
                     if (j > 0) left = dp[i][j - 1];
-                    dp[i][j] = (up + left) % mod;
+                    dp[i][j] = up + left;
                 }
             }
         }
@@ -25,5 +31,3 @@ public class uniquepaths2 {
         return dp[n - 1][m - 1];
     }
 }
-
-    
