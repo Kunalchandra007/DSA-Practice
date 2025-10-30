7a285683eef3735b2439a125f211b4d97944b555
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-10-30 23:45:22 +0530

    Commit

--- Recursion/combinationsum3.java
+++ Recursion/combinationsum3.java
@@ -1,29 +1,26 @@
 package Recursion;
 import java.util.*;
 public class combinationsum3 {
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> result=new ArrayList<>();
-        backtrack(k,n,1,new ArrayList<>(),result);
-        return result;
+    private void helper(int ind, int sum, int n, List<List<Integer>> ans, List<Integer> temp, int k) {
+        if (sum == n && k == 0) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        if (sum > n || k == 0) {
+            return;
+        }
+        for (int i = ind; i <= 9; i++) {
+            if (i > n) break;
+            temp.add(i);
+            helper(i + 1, sum + i, n, ans, temp, k - 1);
+            temp.remove(temp.size() - 1);
+        }
     }
-    private void backtrack(int k,int n,int start,List<Integer> current,List<List<Integer>> result){
-        if (k==0) {
-            if (n==0) {
-                result.add(new ArrayList<>(current));
-            }
-            return;
-        }
-        if (n<0) {
-            return;
-        }
-        for(int i=start;i<=9;i++){
-            if(i>n){//pruning , prevents TLES
-                break;
-            }
-            current.add(i);
-            backtrack(k-1,n-i,i+1,current,result);
-            current.remove(current.size()-1);
-        }
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(1, 0, n, ans, new ArrayList<>(), k);
+        return ans;
     }
 }
     
