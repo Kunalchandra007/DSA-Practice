8501fc3cbbc53c1f01288bc2cfeacc1baf71b717
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-11-03 23:27:03 +0530

    Commit

---
+++ BinaryTrees/Buildtree.java
@@ -0,0 +1,40 @@
+package BinaryTrees;
+public class Buildtree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class Binarytreee{
+        static int idx=-1;
+        public static Node buildTree(int node[]){
+            idx++;
+            if (idx >= node.length || node[idx] == -1) {
+                return null;
+            }
+            Node newNode=new Node(node[idx]);
+            newNode.left=buildTree(node);
+            newNode.right=buildTree(node);
+
+            return newNode;
+
+
+        }
+    }
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6};
+        Binarytreee tree=new Binarytreee();
+        Node root=tree.buildTree(nodes);
+        System.out.println(root.data);
+
+        }
+        
+    }
+    
+
