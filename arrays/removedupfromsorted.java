06e15df2c45f7a4210ca328aafca79b7010581c3
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-11-24 17:22:54 +0530

    Commit

---
+++ arrays/removedupfromsorted.java
@@ -0,0 +1,17 @@
+public class removedupfromsorted {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int n=nums.length;
+        for(int j=1;j<n;j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+
+                i++;
+            }
+        }
+        return i+1;
+        
+    }
+}
+    
+
