186c9cb065e993abcbd2d8cbd9b643e195b8d343
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-12-01 13:48:18 +0530

    Commit

---
+++ arrays/Singlenumber.java
@@ -0,0 +1,18 @@
+public class Singlenumber {
+    public static int Singlenumberfind(int []arr){
+        int xor=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            xor^=arr[i];
+
+        }
+        return xor;
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,1,2,3,3,8,8};
+        System.out.println("Singlenumber is "+Singlenumberfind(arr));
+        
+    }
+    
+}
