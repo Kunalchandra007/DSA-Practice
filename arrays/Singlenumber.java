8925a72962896fdd6cabbd456ad861c8afa99738
Kunalchandra007 <kunalchandra2506@gmail.com>
2025-12-04 21:17:17 +0530

    Commit

--- arrays/Singlenumber.java
+++ arrays/Singlenumber.java
@@ -16,3 +16,14 @@ public class Singlenumber {
     }
     
 }
+//leetcode
+//    public static int Singlenumberfind(int []arr){
+//         int xor=0;
+//         int n=arr.length;
+//         for(int i=0;i<n;i++){
+//             xor^=arr[i];
+
+//         }
+//         return xor;
+
+//     }
