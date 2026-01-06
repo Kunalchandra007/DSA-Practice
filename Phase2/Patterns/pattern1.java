a0cdad87733a9ef155619d91708ca004759a4355
Kunalchandra007 <kunalchandra2506@gmail.com>
2026-01-06 17:02:50 +0530

    Commit

---
+++ Phase2/Patterns/pattern1.java
@@ -0,0 +1,19 @@
+public class pattern1 {
+    public static void pattern1(int n) {
+     // Outer loop will run for rows.
+        for (int i = 0; i < n; i++) {
+            
+            // Inner loop will run for columns.
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            /* As soon as n stars are printed, move
+            to the next row and give a line break. */
+            System.out.println();
+}
+    }
+public static void main(String[] args) {
+        int N = 4;
+        pattern1(N);
+    }
+}
