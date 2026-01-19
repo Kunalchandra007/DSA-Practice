d316539174e6185fc3789fbe6f1f581dc19ec957
Kunalchandra007 <kunalchandra2506@gmail.com>
2026-01-19 15:12:34 +0530

    Commit

---
+++ Phase2/Patterns/pattern13.java
@@ -0,0 +1,31 @@
+public class pattern13 {
+    // Function to print pattern13
+   public static void pattern13(int n) {
+        // starting the number
+        int num = 1;
+
+        // Outer loop for the number of rows.
+        for (int i = 1; i <= n; i++) {
+            
+            /*Inner loop will loop for i times and
+            print numbers increasing by 1 each time*/
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num + 1;
+            }
+            /* As soon as the numbers for each iteration
+            are printed, we move to the next row and give
+            a line break otherwise all numbers would get
+            printed in 1 line*/
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        pattern13(N);
+    }
+}
+
+    
+
---
+++ Phase2/Patterns/pattern14.java
@@ -0,0 +1,25 @@
+class pattern14 {
+    // Function to print pattern14
+   public static  void pattern14(int n) {
+        // Outer loop for the number of rows.
+        for (int i = 0; i < n; i++) {
+            
+            /* Inner loop will loop for i times and
+            print alphabets from A to A + i.*/
+            for (char ch = 'A'; ch <= 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+            
+            /*As soon as the letters for each iteration 
+            are printed, we move to the next row and give
+            a line break otherwise all letters would get
+            printed in 1 line*/
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        pattern14(N);
+    }
+}
